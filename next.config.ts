aa5ccb0490e043f00d88739fb26985674c7c510e
Andreirl032 <andrei.ramos.lopes@gmail.com>
2025-02-02 23:11:24 -0300

    mongodb adicionado

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   env: {
     MONGODB_URI:
-      "mongodb+srv://andrei:a30102004@cluster0.94pxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      "mongodb+srv://andrei:4LiPeHPUkOJQt1jU@cluster0.94pxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   },
 };
 
